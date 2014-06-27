06be19d3b0a8a3098a883ba22933b16596b7ff67
Jonathan Lee <123abcjonathanlee@gmail.com>
2014-06-27 00:26:52 -0700

    edit server port to adapt to heroku

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ app.get('/', function(req, res){
 
 app.use(express.static(__dirname + '/public'));
 
-http.listen(3000, function(){
-    console.log('listening on *:3000');
+http.listen(process.env.PORT ||3000, function(){
+    //console.log('listening on *:3000');
 });
 
 
