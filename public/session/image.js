8b0748115138a634ba15f7395153be03383de740
Jonathan Lee <123abcjonathanlee@gmail.com>
2014-07-23 11:32:41 -0700

    add gitignore file, ignore idea, create image class

---
+++ public/session/image.js
@@ -0,0 +1,13 @@
+function Image(width, height, x, y, tag)
+{
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    this.tag = tag;
+}
+
+Image.prottype.setImage(img)
+{
+    
+}
