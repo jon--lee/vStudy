cf39bbc1efe0bec5fb00201dac715a05a18efd15
Jonathan Lee <123abcjonathanlee@gmail.com>
2014-07-23 23:08:05 -0700

    testing turn

--- public/session/main.js
+++ public/session/main.js
@@ -186,7 +186,8 @@ if(!gettingUserMedia){
 //console.log('Getting user media with constraints', constraints);
 
 if (location.hostname != "localhost") {
-  requestTurn('https://computeengineondemand.appspot.com/turn?username=41784574&key=4080218913');
+  //requestTurn('https://computeengineondemand.appspot.com/turn?username=41784574&key=4080218913');
+    requestTurn('');
 }
 
 function maybeStart() {
