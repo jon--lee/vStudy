1745db34255bbb59cd0e491657663697392e4a40
Jonathan Lee <123abcjonathanlee@gmail.com>
2014-08-11 11:42:33 -0700

    update drawing classes

--- public/session/drawing.js
+++ public/session/drawing.js
@@ -5,9 +5,70 @@ function Drawing(lineColor, lineWidth, composite)
     this.composite = composite;
 }
 
-Rect.prototype = new Drawing();
-Rect.prototype.constructor = Rect;
-function Line(lineColor, lineWidth, composite, startPos, endPos)
+Drawing.prototype.setContextColorWidthComposite = function(context, lineColor, lineWidth, composite){
+    this.context = context;
+    this.lineColor = lineColor;
+    this.lineWidth = lineWidth;
+    this.composite = composite;
+}
+
+Drawing.prototype.drawSelf = function(){
+    alert("drawing self");
+}
+
+Drawing.toJSON = function(){
+    var temp = {
+        "lineColor": this.lineColor,
+        "lineWidth": this.lineWidth,
+        "composite": this.composite
+    };
+    return JSON.stringify(temp);
+}
+
+Line.prototype = Drawing.prototype;
+Line.prototype.constructor = Line;
+function Line(context, lineColor, lineWidth, composite, startPos, endPos)
 {
+    this.setContextColorWidthComposite(context, lineColor, lineWidth, composite);
+    this.startPos = startPos;
+    this.endPos = endPos;
+}
+Line.prototype.drawSelf = function(){
     
 }
+Drawing.toJSON = function(){
+    var temp = {
+        "startPos": this.startPos,
+        "endPos": this.endPos,
+        "lineColor": this.lineColor,
+        "lineWidth": this.lineWidth,
+        "composite": this.composite
+    };
+    return JSON.stringify(temp);
+}
+
+
+
+
+Rect.prototype = Drawing.prototype;
+Rect.prototype.constructor = Rect;
+function Rect(context, lineColor, lineWidth, composite, startPos, dims)
+{
+    this.setContextColorWidthComposite(context, lineColor, lineWidth, composite);
+    this.startPos = startPos;
+    this.endPos = endpos;
+}
+Rect.prototype.drawSelf = function(){
+    
+}
+Drawing.toJSON = function(){
+    var temp = {
+        "startPos": this.startPos,
+        "dims": this.dims,
+        "lineColor": this.lineColor,
+        "lineWidth": this.lineWidth,
+        "composite": this.composite
+    };
+    return JSON.stringify(temp);
+}
+
